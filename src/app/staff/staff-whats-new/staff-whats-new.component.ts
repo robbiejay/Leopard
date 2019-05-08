1157f5e8d7420d202a3a6704b8021dac62349d6e
Robbie Jay <rj1935@my.bristol.ac.uk>
2019-05-09 02:28:23 +0800

    Staff UI Updates

--- src/app/staff/staff-whats-new/staff-whats-new.component.ts
+++ src/app/staff/staff-whats-new/staff-whats-new.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
+import { Post } from '../../_models/post.model';
+import { BlogService } from '../../_services/blog.service';
 @Component({
   selector: 'app-staff-whats-new',
   templateUrl: './staff-whats-new.component.html',
-  styleUrls: ['./staff-whats-new.component.scss']
+  styleUrls: ['./staff-whats-new.component.scss'],
+  providers: [BlogService]
 })
-export class StaffWhatsNewComponent implements OnInit {
-
-  constructor() { }
+export class StaffWhatsNewComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  isLoading = false;
+  private postsSubscription: Subscription;
+  constructor(private bloggingService: BlogService) { }
 
   ngOnInit() {
+    this.isLoading = true;
+    this.bloggingService.getPosts();
+    this.postsSubscription = this.bloggingService.getPostUpdateListener().subscribe((posts: Post[]) => {
+      this.isLoading = false;
+      this.posts = posts;
+    })
+  }
+
+  ngOnDestroy() {
+    this.postsSubscription.unsubscribe();
   }
 
 }
